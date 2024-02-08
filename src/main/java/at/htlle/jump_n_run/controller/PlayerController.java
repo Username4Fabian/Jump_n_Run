847f1c8faefa317b71c7a9ccf1a51144e23e6160
Username4Fabian <03anion.proxys@icloud.com>
2024-02-08 14:43:49 +0100

    Add Spring Security Crypto dependency and encrypt player passwords

--- src/main/java/at/htlle/jump_n_run/controller/PlayerController.java
+++ src/main/java/at/htlle/jump_n_run/controller/PlayerController.java
@@ -1,14 +1,13 @@
 package at.htlle.jump_n_run.controller;
 
 import java.time.LocalDate;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import at.htlle.jump_n_run.models.Player;
 import at.htlle.jump_n_run.repositories.PlayerRepository;
 import at.htlle.jump_n_run.repositories.ScoreRepository;
@@ -17,15 +16,17 @@ import at.htlle.jump_n_run.repositories.ScoreRepository;
 public class PlayerController {
     @Autowired
     private PlayerRepository playerRepository;
-    @SuppressWarnings("unused")
     @Autowired
+    @SuppressWarnings("unused")
     private ScoreRepository scoreRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @PostMapping("/createPlayer")
     public ResponseEntity<Object> createPlayer(@RequestParam String name, @RequestParam String password) {
         Player existingPlayer = playerRepository.findByName(name);
         if(existingPlayer != null) {
-            if(existingPlayer.getPassword().equals(password)) {
+            if(bCryptPasswordEncoder.matches(password, existingPlayer.getPassword())) {
                 return new ResponseEntity<>(existingPlayer.getName(), HttpStatus.CONFLICT);
             } else {
                 return new ResponseEntity<>("Username already taken", HttpStatus.BAD_REQUEST);
@@ -33,7 +34,7 @@ public class PlayerController {
         }
         Player player = new Player();
         player.setName(name);
-        player.setPassword(password);
+        player.setPassword(bCryptPasswordEncoder.encode(password));
         player.setCreationDate(LocalDate.now());
         player.setHighscore(0);
         player.setTotalPlaytime(java.sql.Time.valueOf("00:00:00"));
@@ -41,5 +42,4 @@ public class PlayerController {
     
         return new ResponseEntity<>("Player created", HttpStatus.OK);
     }
-
 }
